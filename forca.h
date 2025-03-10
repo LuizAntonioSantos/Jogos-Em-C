4c2412861427ab55e0e407933e0423ea0e3e5c8c
Luiz Antonio <luizantonio2mil5@gmail.com>
2025-03-10 15:28:49 -0300

    Melhorando o jogo da forca

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA = 20;
+
+int enforcou();
+int ganhou();
+int jachutou(char letra);
+int chuteserrados();
+int letraexiste();
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
